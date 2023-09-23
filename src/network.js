33d0109b21ec99d1c56d26a248063a6b4f41ec16
Sandy F Cortes <sandra_hakufu@hotmail.com>
2023-09-23 15:42:23 -0600

    Update network.js

--- src/network.js
+++ src/network.js
@@ -1,4 +1,4 @@
-const url="https://preapi.wex.services"
+const url="https://ginawex.ngrok.dev"
 const myHeaders = new Headers();
 
 myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
